575498e6422c161db4bb5f15febcdcf642aea7f5
assfa709 <asseb709@gmail.com>
2025-05-05 12:06:55 +0300

    js variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('test');
+
+//We can use let to reassign variable
+let a = 1;
+
+//const used only for constant variables
+const pi = 3.14;
+let b = "abebe";
+let c = true;
+let d = 'a';
+
+console.log(a);
