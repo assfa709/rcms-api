10bf3a663a08eb5825b6459ede22f5609266a42e
assfa709 <asseb709@gmail.com>
2025-05-05 12:29:42 +0300

    js functions

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ let c = true;
 let d = 'a';
 
 console.log(a);
+
+if (a === 2){
+    console.log('correct');
+}else{
+    console.log('incorrect');
+}
+
+const arr = [1, 2, 3, 4, 5];
+for (let i=0; i<arr.length; i++){
+    console.log(`number: ${i + 1}`)
+}
+
+
+function addition(a, b){
+    return a + b;
+}
+
+
+console.log(addition(4, 5));
+
+
